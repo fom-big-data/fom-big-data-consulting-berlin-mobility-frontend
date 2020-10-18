90c8227d795429960026c81271f3831803290103
Florian Schwanz <florian.schwanz@interoberlin.de>
2020-10-18 12:45:27 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'fom-big-data-consulting-berlin-mobility-frontend';
+}
