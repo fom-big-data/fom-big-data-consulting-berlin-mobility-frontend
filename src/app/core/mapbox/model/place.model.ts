d67ef03796f6963a4a1b7db4aec51368371b58e5
Florian Schwanz <florian.schwanz@interoberlin.de>
2020-11-20 21:12:11 +0100

    Add new places

--- src/app/core/mapbox/model/place.model.ts
+++ src/app/core/mapbox/model/place.model.ts
@@ -6,4 +6,6 @@ import {Location} from './location.model';
 export class Place {
   static BRANDENBURG_GATE = new Location(13.377777777778, 52.516388888889);
   static BRANDENBURG_GATE_STATION = new Location(13.380833333333,52.516388888889);
+  static ARD_HAUPTSTADTSTUDIO = new Location(13.380343, 52.518697);
+  static ARD_HAUPTSTADTSTUDIO_2 = new Location(13.38, 52.6);
 }
