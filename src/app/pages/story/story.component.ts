66c1b8037faedabead6fd49619800512969494ff
Florian Schwanz <florian.schwanz@interoberlin.de>
2020-11-28 03:20:44 +0100

    Make toolbar sticky #21

--- src/app/pages/story/story.component.ts
+++ src/app/pages/story/story.component.ts
@@ -1,13 +1,10 @@
-import {Component, Directive, Input, QueryList, ViewChildren} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, OnDestroy, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {MapBoxStyle} from '../../core/mapbox/model/map-box-style.enum';
 import {Place} from '../../core/mapbox/model/place.model';
 import {SectionHeaderComponent} from './components/section-header/section-header.component';
 import {ViewportScroller} from '@angular/common';
-
-@Directive({selector: 'app-section-header'})
-export class SelectionHeaderDirective {
-  @Input() id!: string;
-}
+import {fromEvent, Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 
 /**
  * Displays a story
@@ -17,21 +14,65 @@ export class SelectionHeaderDirective {
   templateUrl: './story.component.html',
   styleUrls: ['./story.component.scss']
 })
-export class StoryComponent {
+export class StoryComponent implements AfterViewInit, OnDestroy {
 
   /** Map of all geojsons */
   @Input() geojsons = new Map<string, any>();
-  /** List of section headers */
+
+  /** Toolbar component */
+  @ViewChild('toolbar', {read: ElementRef}) toolbar: ElementRef;
+  /** List of section header components */
   @ViewChildren(SectionHeaderComponent) sectionHeaders: QueryList<SectionHeaderComponent>;
+  /** Whether or not the toolbar should be sticky */
+  toolbarSticky = false;
 
   /** Enum representing map box style */
   mapBoxStyleEnum = MapBoxStyle;
   /** Enum representing place */
   placeEnum = Place;
 
+  /** Helper subject used to finish other subscriptions */
+  private unsubscribeSubject = new Subject();
+
+  /**
+   * Constructor
+   * @param viewportScroller viewport scroller
+   */
   constructor(private viewportScroller: ViewportScroller) {
   }
 
+  //
+  // Lifecycle hooks
+  //
+
+  /**
+   * Handles after-view-init phase
+   */
+  ngAfterViewInit() {
+    const toolbarOffsetTop = this.toolbar.nativeElement.offsetTop;
+    const headerTopRowHeight = 26;
+
+    // Subscribe scroll events
+    fromEvent(window, 'scroll')
+      .pipe(takeUntil(this.unsubscribeSubject))
+      .subscribe((e: Event) => {
+
+        // Make toolbar sticky if user scrolled far enough
+        this.toolbarSticky = window.scrollY > toolbarOffsetTop - headerTopRowHeight;
+      });
+  }
+
+  /**
+   * Handles on-destroy phase
+   */
+  ngOnDestroy() {
+    this.unsubscribeSubject.next();
+  }
+
+  /**
+   * Scrolls to an element
+   * @param anchor anchor
+   */
   scrollToElement(anchor): void {
     console.log(anchor);
     this.viewportScroller.scrollToAnchor(anchor);
