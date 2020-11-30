03dddac7a2898a986e1be5af4e960efbbb9b915e
Florian Schwanz <florian.schwanz@interoberlin.de>
2020-11-30 16:34:03 +0100

    Unify scrolling behavior

--- src/app/pages/story/components/footer/footer.component.ts
+++ src/app/pages/story/components/footer/footer.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input, QueryList} from '@angular/core';
+import {Component, Inject, Input, QueryList} from '@angular/core';
 import {SectionHeaderComponent} from '../section-header/section-header.component';
+import {DOCUMENT, ViewportScroller} from '@angular/common';
 
 /**
  * Displays page footer
@@ -13,4 +14,23 @@ export class FooterComponent {
 
   /** List of section header components */
   @Input() sectionHeaders: QueryList<SectionHeaderComponent>;
+
+  /**
+   * Constructor
+   * @param viewportScroller viewport scroller
+   */
+  constructor(private viewportScroller: ViewportScroller) {
+  }
+
+  //
+  // Helpers
+  //
+
+  /**
+   * Scrolls to an element
+   * @param anchor anchor
+   */
+  scrollToElement(anchor) {
+    this.viewportScroller.scrollToAnchor(anchor);
+  }
 }
