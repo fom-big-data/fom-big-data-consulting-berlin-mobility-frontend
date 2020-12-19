ac7ff1b1d64f40139bedfb7c9c4cd86e2bf17e7c
Florian Schwanz <florian.schwanz@interoberlin.de>
2020-12-19 22:45:32 +0100

    Update color ramp #40

--- src/app/ui/map/model/color-ramp.model.ts
+++ src/app/ui/map/model/color-ramp.model.ts
@@ -5,13 +5,13 @@ export class ColorRamp {
 
   /** Color ramp used on luftdaten.info */
   static LUFTDATEN_COLOR_RAMP = [
-    'rgba(150, 0, 132, 0.6)',
-    'rgba(236, 107, 11, 0.6)',
-    'rgba(246, 155, 31, 0.6)',
-    'rgba(249, 168, 37, 0.6)',
-    'rgba(199, 159, 51, 0.6)',
-    'rgba(137, 147, 69, 0.6)',
+    'transparent',
+    'rgba(236, 107, 11, 0.35)',
+    'rgba(246, 155, 31, 0.4)',
+    'rgba(249, 168, 37, 0.45)',
+    'rgba(199, 159, 51, 0.5)',
+    'rgba(137, 147, 69, 0.55)',
     'rgba(125, 145, 72, 0.6)',
-    'rgba(12, 123, 104, 0.6)'
+    'rgba(12, 123, 104, 0.65)'
   ];
 }
