f7f86f91f917bd17ad9416f30738d92e4cc9eaa2
Florian Schwanz <florian.schwanz@interoberlin.de>
2020-12-21 20:51:04 +0100

    Add more specific color ramps

--- src/app/ui/map/model/color-ramp.model.ts
+++ src/app/ui/map/model/color-ramp.model.ts
@@ -5,13 +5,37 @@ export class ColorRamp {
 
   /** Color ramp used on luftdaten.info */
   static LUFTDATEN_COLOR_RAMP = [
-    'transparent',
-    'rgba(236, 107, 11, 0.35)',
-    'rgba(246, 155, 31, 0.4)',
-    'rgba(249, 168, 37, 0.45)',
-    'rgba(199, 159, 51, 0.5)',
-    'rgba(137, 147, 69, 0.55)',
+    'rgba(150, 0, 132, 0.6)',
+    'rgba(236, 107, 11, 0.6)',
+    'rgba(246, 155, 31, 0.6)',
+    'rgba(249, 168, 37, 0.6)',
+    'rgba(199, 159, 51, 0.6)',
+    'rgba(137, 147, 69, 0.6)',
     'rgba(125, 145, 72, 0.6)',
-    'rgba(12, 123, 104, 0.65)'
+    'rgba(12, 123, 104, 0.6)'
+  ];
+
+  /** Color ramp used on luftdaten.info */
+  static LUFTDATEN_COLOR_RAMP_LIGHT = [
+    'transparent',
+    'rgba(236, 107, 11, 0.05)',
+    'rgba(246, 155, 31, 0.1)',
+    'rgba(249, 168, 37, 0.1)',
+    'rgba(199, 159, 51, 0.2)',
+    'rgba(137, 147, 69, 0.2)',
+    'rgba(125, 145, 72, 0.3)',
+    'rgba(12, 123, 104, 0.3)'
+  ];
+
+  /** Color ramp used on luftdaten.info */
+  static LUFTDATEN_COLOR_RAMP_INVERTED = [
+    'rgba(150, 0, 132, 0.8)',
+    'rgba(236, 107, 11, 0.3)',
+    'rgba(246, 155, 31, 0.25)',
+    'rgba(249, 168, 37, 0.2)',
+    'rgba(199, 159, 51, 0.15)',
+    'rgba(137, 147, 69, 0.1)',
+    'rgba(125, 145, 72, 0.05)',
+    'rgba(12, 123, 104, 0.0025)'
   ];
 }
